4fdbad2881e074331a0075210b737e658ae0905f
ljaviertovar <luisjavier_tovar@outlook.com>
2021-04-01 16:22:38 -0600

    :sparkles: [ADD] Form component

---
+++ src/components/Form.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+
+        <div className="bg-info">
+            <div className="container">
+                <div className="row">
+                    <form
+                        className="col card text-white bg-transparent mb-5 pt-5 pb-2"
+                    >
+                        <fieldset>
+                            <legend className="text-center">Browser Lyrics</legend>
+
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Artist</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="artist"
+                                            placeholder="Artist name"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Song</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="song"
+                                            placeholder="Song name"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="btn btn-primary float-right"
+                            >
+                                Search
+                            </button>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default Form;
