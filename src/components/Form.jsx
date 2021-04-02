1ed47d1528b44bf88db82757044dc09a40613d4f
ljaviertovar <luisjavier_tovar@outlook.com>
2021-04-01 22:23:17 -0600

    :hammer: [UPDATE] Validate Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+
+    const [search, setSearch] = useState({
+        artist: '',
+        song: ''
+    });
+    const [error, setError] = useState(false);
+
+
+    const { artist, song } = search;
+
+    const handleSearch = e => {
+
+        setSearch({
+            ...search,
+            [e.target.name]: e.target.value
+        });
+
+    }
+
+    const searchLyric = e => {
+        e.preventDefault();
+
+        if (artist.trim() === '' || song.trim() === '') {
+            setError(true);
+            return;
+        }
+
+        setError(false);
+
+    }
+
     return (
 
         <div className="bg-info">
             <div className="container">
+
+                {error
+                    ? <p className="alert alert-danger text-center p-2">All fields are require.</p>
+                    : null
+                }
+                
                 <div className="row">
+
                     <form
                         className="col card text-white bg-transparent mb-5 pt-5 pb-2"
+                        onSubmit={searchLyric}
                     >
                         <fieldset>
                             <legend className="text-center">Browser Lyrics</legend>
@@ -21,6 +60,8 @@ const Form = () => {
                                             className="form-control"
                                             name="artist"
                                             placeholder="Artist name"
+                                            onChange={handleSearch}
+                                            value={artist}
                                         />
                                     </div>
                                 </div>
@@ -32,6 +73,8 @@ const Form = () => {
                                             className="form-control"
                                             name="song"
                                             placeholder="Song name"
+                                            onChange={handleSearch}
+                                            value={song}
                                         />
                                     </div>
                                 </div>
